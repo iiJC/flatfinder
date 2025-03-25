b0851c58ff2a543d83784727bf9bf80a494b3f8b
iiJC <jonathanchan1249@gmail.com>
2025-03-25 23:46:06 +1300

    deployment fix

--- app/flats/[id]/page.js
+++ app/flats/[id]/page.js
@@ -1,3 +1,13 @@
-export default function FlatsPage() {
-    return <h2 className="text-2xl font-semibold">Browse Available Flats</h2>;
+// TODO - setup mongodb vvv 
+// import clientPromise from "@/lib/mongodb";
+
+export default function FlatDetailsPage({ params }) {
+    const { id } = params;
+  
+    return (
+      <div>
+        <h2 className="text-2xl font-bold mb-4">Flat Details</h2>
+        <p className="text-lg">You're viewing flat: <strong>{id}</strong></p>
+      </div>
+    );
   }
