e22b13b669212e378952663434f126c51d970a1c
iiJC <jonathanchan1249@gmail.com>
2025-03-25 23:37:40 +1300

    page structure, mongodb/mongoose

---
+++ app/apply/page.js
@@ -0,0 +1,3 @@
+export default function ApplyPage() {
+    return <h2 className="text-2xl font-semibold">Apply for a Flat</h2>;
+  }
---
+++ app/register/page.js
@@ -0,0 +1,3 @@
+export default function RegisterPage() {
+    return <h2 className="text-2xl font-semibold">Register</h2>;
+  }
