3ec1e9e8dae83e4548b0196e32e8fa5d297b8066
Ben Hunt <hunbe833@student.otago.ac.nz>
2025-03-27 14:39:55 +1300

    Implemented map with location pointing to Dunedin

--- app/map/page.js
+++ app/map/page.js
@@ -13,18 +13,17 @@ export default function MapPage() {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-74.5, 40],
+      center: [170.5028, -45.8788],
       zoom: 9
     });
     
-    // Clean up on unmount
     return () => map.remove();
-  }, []); // Empty dependency array means this effect runs once on mount
+  }, []);
   
   return (
     <div>
       <h2 className="text-2xl font-semibold">Map View</h2>
-      <div id="map" ref={mapContainerRef} style={{ height: '400px', width: '100%' }} />
+      <div id="map" ref={mapContainerRef} style={{ height: '700px', width: '100%' }} />
     </div>
   );
 }
