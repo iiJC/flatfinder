8eaa98ba234cbe88d6b02de5e9bf7757b3a3f463
Amelia Johnston <joham375@student.otago.ac.nz>
2025-03-30 23:14:57 +1300

    forgot to uncomment

--- app/map/page.js
+++ app/map/page.js
@@ -6,108 +6,108 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 export default function MapPage() {
-  // const mapContainerRef = useRef(null);
-  // const [map, setMap] = useState(null);
-  // const [menuVisible, setMenuVisible] = useState(false);
+  const mapContainerRef = useRef(null);
+  const [map, setMap] = useState(null);
+  const [menuVisible, setMenuVisible] = useState(false);
 
-  // useEffect(() => {
-  //   mapboxgl.accessToken = "pk.eyJ1IjoiaHVuYmU4MzMiLCJhIjoiY204cGQ3MTBzMGEyeTJpcTB4ZWJodHdpNSJ9.Y3jD8AYlV8fY3TKp3RHccg";
+  useEffect(() => {
+    mapboxgl.accessToken = "pk.eyJ1IjoiaHVuYmU4MzMiLCJhIjoiY204cGQ3MTBzMGEyeTJpcTB4ZWJodHdpNSJ9.Y3jD8AYlV8fY3TKp3RHccg";
 
-  //   const initializedMap = new mapboxgl.Map({
-  //     container: mapContainerRef.current,
-  //     style: "mapbox://styles/mapbox/streets-v11",
-  //     center: [170.5028, -45.8788],
-  //     zoom: 12,
-  //     pitch: 0,
-  //     bearing: 0,
-  //     antialias: true,
-  //   });
+    const initializedMap = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [170.5028, -45.8788],
+      zoom: 12,
+      pitch: 0,
+      bearing: 0,
+      antialias: true,
+    });
 
-  //   setMap(initializedMap);
+    setMap(initializedMap);
 
-  //   initializedMap.on("load", () => {
-  //     initializedMap.addLayer({
-  //       id: "terrain",
-  //       type: "raster-dem",
-  //       source: {
-  //         type: "raster",
-  //         url: "mapbox://mapbox.terrain-rgb",
-  //         tileSize: 512,
-  //       },
-  //       minzoom: 0,
-  //       maxzoom: 13,
-  //     });
+    initializedMap.on("load", () => {
+      initializedMap.addLayer({
+        id: "terrain",
+        type: "raster-dem",
+        source: {
+          type: "raster",
+          url: "mapbox://mapbox.terrain-rgb",
+          tileSize: 512,
+        },
+        minzoom: 0,
+        maxzoom: 13,
+      });
 
-  //     initializedMap.addLayer({
-  //       id: "3d-buildings",
-  //       type: "fill-extrusion",
-  //       source: {
-  //         type: "vector",
-  //         url: "mapbox://mapbox.buildings",
-  //       },
-  //       "source-layer": "building",
-  //       paint: {
-  //         "fill-extrusion-color": "#aaa",
-  //         "fill-extrusion-height": ["get", "height"],
-  //         "fill-extrusion-base": ["get", "min_height"],
-  //       },
-  //     });
-  //   });
+      initializedMap.addLayer({
+        id: "3d-buildings",
+        type: "fill-extrusion",
+        source: {
+          type: "vector",
+          url: "mapbox://mapbox.buildings",
+        },
+        "source-layer": "building",
+        paint: {
+          "fill-extrusion-color": "#aaa",
+          "fill-extrusion-height": ["get", "height"],
+          "fill-extrusion-base": ["get", "min_height"],
+        },
+      });
+    });
 
-  //   const marker = new mapboxgl.Marker({
-  //     color: "#314ccd",
-  //   });
+    const marker = new mapboxgl.Marker({
+      color: "#314ccd",
+    });
 
-  //   marker.setLngLat([170.5028, -45.8788]).addTo(initializedMap);
+    marker.setLngLat([170.5028, -45.8788]).addTo(initializedMap);
 
-  //   const geolocate = new mapboxgl.GeolocateControl({
-  //     positionOptions: {
-  //       enableHighAccuracy: true,
-  //     },
-  //     trackUserLocation: true,
-  //     showUserLocation: true,
-  //   });
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+      showUserLocation: true,
+    });
 
-  //   initializedMap.addControl(geolocate);
-  //   geolocate.trigger();
+    initializedMap.addControl(geolocate);
+    geolocate.trigger();
 
-  //   const geocoder = new MapboxGeocoder({
-  //     accessToken: mapboxgl.accessToken,
-  //     mapboxgl: mapboxgl,
-  //     marker: {
-  //       color: "#314ccd",
-  //     },
-  //     placeholder: "Search for a place...",
-  //     proximity: { longitude: 170.5028, latitude: -45.8788 },
-  //   });
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      marker: {
+        color: "#314ccd",
+      },
+      placeholder: "Search for a place...",
+      proximity: { longitude: 170.5028, latitude: -45.8788 },
+    });
 
-  //   initializedMap.addControl(geocoder);
+    initializedMap.addControl(geocoder);
 
-  //   return () => initializedMap.remove();
-  // }, []);
+    return () => initializedMap.remove();
+  }, []);
 
-  // const changeMapStyle = (style) => {
-  //   if (map) {
-  //     map.setStyle(style);
-  //   }
-  // };
+  const changeMapStyle = (style) => {
+    if (map) {
+      map.setStyle(style);
+    }
+  };
 
-  // const toggleMenu = () => {
-  //   setMenuVisible(!menuVisible);
-  // };
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
 
-  // <div>
-  //   <h2 className="text-2xl font-semibold">Map View</h2>
-  //   <div className="hamburger-menu" onClick={toggleMenu}>
-  //     ☰
-  //   </div>
-  //   {menuVisible && (
-  //     <div className="menu">
-  //       <button onClick={() => changeMapStyle("mapbox://styles/mapbox/satellite-v9")}>Satellite View</button>
-  //       <button onClick={() => changeMapStyle("mapbox://styles/mapbox/streets-v11")}>Street View</button>
-  //       <button onClick={() => changeMapStyle("mapbox://styles/mapbox/outdoors-v11")}>Outdoor View</button>
-  //     </div>
-  //   )}
-  //   <div id="map" ref={mapContainerRef} style={{ height: "700px", width: "100%" }} />
-  // </div>;
+  <div>
+    <h2 className="text-2xl font-semibold">Map View</h2>
+    <div className="hamburger-menu" onClick={toggleMenu}>
+      ☰
+    </div>
+    {menuVisible && (
+      <div className="menu">
+        <button onClick={() => changeMapStyle("mapbox://styles/mapbox/satellite-v9")}>Satellite View</button>
+        <button onClick={() => changeMapStyle("mapbox://styles/mapbox/streets-v11")}>Street View</button>
+        <button onClick={() => changeMapStyle("mapbox://styles/mapbox/outdoors-v11")}>Outdoor View</button>
+      </div>
+    )}
+    <div id="map" ref={mapContainerRef} style={{ height: "700px", width: "100%" }} />
+  </div>;
 }
