d664f60ac368366258bf3d0293c103769aed78f1
Amelia Johnston <joham375@student.otago.ac.nz>
2025-04-03 10:21:43 +1300

    may need to change the root layout page

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,6 @@ export default function RootLayout({ children }) {
           <div className="search-bar">
             <input type="text" placeholder="Search for flats or flatmates..." />
           </div>
-<<<<<<< HEAD
           
           <nav className="space-x-4">
             <Link href="/flats" className="text-white hover:underline">Flats</Link>
@@ -28,14 +27,6 @@ export default function RootLayout({ children }) {
             <Link href="/login" className="text-white hover:underline">Login</Link>
             <Link href="/addflat" className="text-white hover:underline">add flat</Link>
           
-=======
-
-          <nav className="nav-links">
-            <Link href="/flats">Flats</Link>
-            <Link href="/map">Map</Link>
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/login">Login</Link>
->>>>>>> b2ebd7fbc9fc1da58ed4526a5fe20c74a33fd557
           </nav>
         </header>
 
