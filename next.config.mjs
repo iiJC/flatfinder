a415ddfa7dc5f471ee72794fc2adb34b2307aded
iiJC <jonathanchan1249@gmail.com>
2025-04-11 12:18:03 +1200

    opening to standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export" // switch to standalone for server-side features (e.g. MongoDB, APIs)
+  output: "standalone" // switch to standalone for server-side features (e.g. MongoDB, APIs)
 };
 
 export default nextConfig;
