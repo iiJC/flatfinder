9ff5ec54266dff78b11669b1f45bf96dfef852a1
Ben Hunt <hunbe833@student.otago.ac.nz>
2025-05-15 11:39:16 +1200

    Unit Test Fixing

--- __tests__/MapPage.test.js
+++ __tests__/MapPage.test.js
@@ -7,3 +7,14 @@ describe("MapPage", () => {
     expect(screen.getByText(/Filter Flats/i)).toBeInTheDocument();
   });
 });
+
+jest.mock('@mapbox/mapbox-gl-geocoder', () => {
+  return jest.fn().mockImplementation(() => ({
+    onAdd: jest.fn(() => document.createElement('div')),
+    on: jest.fn(),
+    off: jest.fn(),
+    addTo: jest.fn(),
+    remove: jest.fn(),
+  }));
+});
+
